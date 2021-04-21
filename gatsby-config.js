ddb02e78d62525393a5e8227b18e95c79ad3d1af
Niamh McCooey <niamhmccooey@gmail.com>
2021-04-21 18:11:53 +0100

    feat: set up site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
-  siteMetadata: {
-    title: "tides",
-  },
-  plugins: [],
+	siteMetadata: {
+		title: 'tides',
+		tidesApiKey: process.env.TIDES_API_KEY,
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+	],
 };
